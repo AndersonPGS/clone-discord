943d493a6d50007a17405df77754ec068181d6a3
AndersonPGS <andersonpgs2001@hotmail.com>
2021-03-07 16:19:40 -0300

    ChannelData and ChannelMessages created. Projected Finished

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import ChannelMessage, { Mention } from '../ChannelMessage';
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
 
 const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef])
+
   return (
     <Container>
-      <Messages />
+      <Messages ref={messagesRef}>
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage 
+            author="Anderson Paiva"
+            date="20/11/2021"
+            content="Hello World KKKKKKKKK"
+          />
+        ))}
+        
+
+        <ChannelMessage 
+          author="Jefersu"
+          date="20/11/2021"
+          content={
+            <>
+              <Mention>@Anderson Paiva</Mention> 🦧🙈
+            </>
+          }
+          hasMention
+          isBot
+        /> 
+      </Messages>
 
       <InputWrapper>
         <Input type="text" placeholder="Conversar em #chat-livre"/>
