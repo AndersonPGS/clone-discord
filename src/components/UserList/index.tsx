91c29d73b3172a03a2a81b64387f7bd98809cb26
AndersonPGS <andersonpgs2001@hotmail.com>
2021-03-07 14:32:11 -0300

    UserList added and completed, ChannelData component created

---
+++ src/components/UserList/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import { Container, Role, User, Avatar } from './styles';
+
+interface UserProps{
+  nickname: string;
+  isBot?: boolean;
+  isOffline?: boolean;
+}
+
+const UserRow: React.FC<UserProps> = ({ nickname, isBot, isOffline}) => {
+  return (
+    <User>
+      <Avatar className={isBot ? 'bot' : ''}/>
+
+      <strong className={isOffline ? 'offline' : ''}>{nickname}</strong>
+
+      {isBot && <span>Bot</span>}
+    </User>
+  );
+}
+
+const UserList: React.FC = () => {
+
+  return (
+    <Container>
+      <Role>Disponível - 1</Role>
+      <UserRow nickname="Anderson Paiva" />
+
+      <Role>Offline - 30</Role>
+      <UserRow nickname="Filho do Paiva" isOffline isBot/>
+      <UserRow nickname="Jefesu" isOffline isBot/>
+      <UserRow nickname="Namthanael" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Táliçõn" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+      <UserRow nickname="Paiva" isOffline/>
+    </Container>
+  );
+};
+
+export default UserList;
